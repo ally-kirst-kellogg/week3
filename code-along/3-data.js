ec49e913ca522fa7809780b071a8b6999e5ac39a
ally-kirst-kellogg <ally.kirst@kellogg.northwestern.edu>
2021-04-13 10:54:50 -0500

    curshing it

--- code-along/3-data.js
+++ code-along/3-data.js
@@ -10,16 +10,34 @@
 // Store a simple social media profile in memory
 let profile = {
     name: `Ally`,
-    location: `Chicago`
+    location: {
+        city: `Chicago`,
+        state: `Illinois`
+    },
+    timeline: [`Learning how to code`, `Eating Tacos`, `Getting a dog`]
 }
+console.log(profile)
 
 // Retrieve the name from the profile, store it in memory
+let profileName = profile.name
 
 // Display the name in the console
+console.log(`Name: ${profileName}`)
 
 // Retrieve the location, store it in a memory
+let profileLocation = profile.location
 
 // Use the location to write the city and state, in a human-readable
 // format, to the JavaScript console
 
+//console.log(`Location: ${profileLocation.city}, ${profileLocation.state}`)
+console.log(`Location: ${profile.location.city}, ${profile.location.state}`)
+
+//list 
+let burger = [`beef`, `bun`, `cheese`, `speical sauce`]
+
 // Write the 3 statuses from the "timeline" to the JavaScript console
+console.log(`Timeline:`)
+console.log(profile.timeline[0])
+console.log(profile.timeline[1])
+console.log(profile.timeline[2])
--- lab/week3.js
+++ lab/week3.js
@@ -31,41 +31,101 @@ document.addEventListener('DOMContentLoaded', async function() {
 
   // PART 1
   // Figure out how products there are, and store that number in memory
+  let numProduct =data.products.length
 
   // Figure out how promotions there are, and store that number in memory
+   let numPromotions =data.promotions.length
 
+  
   // Write the number of products to the JavaScript console
+   console.log(`${numProduct}`)
 
   // Write the number of promotions to the JavaScript console
 
+   console.log(numPromotions)
+
   // PART 2
   // Store the first product in memory
+  let product1 =data.products[0]
 
   // Write the first product to the JavaScript console, on three separate lines
 
+  console.log(product1)
+  console.log(product1.name)
+  console.log(product1.description)
+  console.log(product1.price)
+
   // Repeat for the 2nd and 3rd products
+
   // Store the second product in memory
 
   // Write the product to the JavaScript console, on three separate lines
-  
+
+ let product2 =data.products[1]
+ console.log(product2.name)
+ console.log(product2.description)
+ console.log(product2.price)
+
   // Store the third product in memory
 
   // Write the product to the JavaScript console, on three separate lines
 
+ let product3 =data.products[2]
+ console.log(`Name: ${product3.name}`)
+ console.log(product3.description)
+ console.log(product3.price)
+
+
   // PART 3
+  // - PART 3: Get a random promotion from the list of promotions. Display the description of the
+  //   promotion, along with the appropriate emoji.
+  //   - Discount = 🤑
+  //   - Shipping = 🚛
+  //   - Contest = 🏆
+
   // Get a random integer between 1 and 10
-
-  // Subtract one from that random integer because Arrays are zero-based
-
+  
+  let randomNumber = getRandomInt(10)
+   console.log(randomNumber)
+  
+   // Subtract one from that random integer because Arrays are zero-based
+  
+   let randomNumberArray = randomNumber - 1
+   console.log(randomNumberArray)
+ 
   // Use the random integer to grab a promotion from the Array of promotions
   // and store it in memory
-
+  let randomPromotion = data.promotions[randomNumberArray]
+  console.log(randomPromotion)
+  //console.log(randomPromotion.type)
   // Figure out which emoji should be displayed, store it in memory
+
+  let randomPromotionType = randomPromotion.type
+  console.log(randomPromotionType)
+ 
+  if (randomPromotion.type == `shipping`){
+    console.log(`🚛`)}
+  else if (randomPromotion.type == `discount`){
+      console.log(`🤑`)}
+  else if (randomPromotion.type == `contest`){
+      console.log(`🏆`)}
+  else {
+      console.log(`Sorry!`)}
 
   // Create a sentence that displays the emoji along with the description 
   // of the promotion
 
   // Write the sentence to the JavaScript console
 
+  if (randomPromotion.type == `shipping`){
+    console.log(`Shipping 🚛: Description: ${randomPromotion.description}`)}
+  else if (randomPromotion.type == `discount`){
+      console.log(`Discount 🤑 Description: ${randomPromotion.description}`)}
+  else if (randomPromotion.type == `contest`){
+      console.log(`Discount 🏆 Description: ${randomPromotion.description}`)}
+  else {
+      console.log(`Sorry!`)}
+
   // 🔥 YOUR CODE ENDS HERE 🔥
-})
+}
+)
